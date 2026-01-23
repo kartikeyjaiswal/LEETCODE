d4b18d35af6a0ba99bff9248a19f2b851c82f67b
Kartikey jaiswal <121501044+kartikeyjaiswal@users.noreply.github.com>
2026-01-23 22:34:37 +0530

    Time: 39 ms (77.96%), Space: 53.2 MB (85.22%) - LeetHub

---
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -0,0 +1,15 @@
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+
+Array.prototype.last = function() {
+    return this.length > 0 ? this[this.length - 1] : -1;
+};
+
+/*
+const arr = [1, 2, 3];
+arr.last(); // 3
+
+const empty = [];
+empty.last(); // -1
+*/
