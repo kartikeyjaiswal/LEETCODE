3567ee2c15393e12ebb1c0ce2714994537c6ed8a
Kartikey jaiswal <121501044+kartikeyjaiswal@users.noreply.github.com>
2026-01-24 20:05:25 +0530

    Time: 172 ms (11.57%), Space: 91.7 MB (36.68%) - LeetHub

---
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Array} arr
+ * @param {number} depth
+ * @return {Array}
+ */
+var flat = function (arr, n) {
+    if(n === 0){
+        return arr.slice()
+    }
+    let res= []
+    for(let i = 0;i<arr.length;i++){
+        if(Array.isArray(arr[i])){
+            const a = flat(arr[i],n-1)
+            res.push(...a)                      
+        }else{
+            res.push(arr[i])
+        }
+    }return res
+};
